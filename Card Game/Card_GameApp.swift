8c43c98808e4cda2600eececb56d00d9e99153d9
vijayulektz <vijay.v@ulektz.in>
2023-06-06 17:00:22 +0530

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by ulektz on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
